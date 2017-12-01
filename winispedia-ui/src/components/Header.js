1a19fe35b50771888f9c92053d09a89ab090387a
Ryan Rodwell <ryanrodwell@gmail.com>
2017-11-30 21:27:08 -0500

    testing header component

--- winispedia-ui/src/components/Header.js
+++ winispedia-ui/src/components/Header.js
@@ -1,4 +1,23 @@
 import React, { Component } from 'react';
-import { TeamLogo, Wins, Ties, WinLossBar, Streak } from './../subcomponents/header';
+import { TeamLogo } from '../subcomponents/header';
 
+
+export default class Header extends Component {
+ 
+    static defaultProps = {
+        slug: "georgia-tech",
+        schoolName: "Georgia Tech",
+    }
+
+
+    render() {
+        return (
+            <div>
+                <TeamLogo/>
+            </div>
+        )
+    }
+
+
+}
 
--- winispedia-ui/src/subcomponents/header/TeamLogo.js
+++ winispedia-ui/src/subcomponents/header/TeamLogo.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
 
 export default class TeamLogo extends Component {
-
-  static defaultProps = {
-    slug: "georgia-tech",
-    schoolName: "Georgia Tech",
-  }
-
-  const testName = "ryan";
-  
-  constructor(){
-    super(props);
-  }
   
   render() {
     return (
       <div>
-        <img className="teamLogo" src={require(`../public/images/Team_logos/${slug}.120.png`)} alt={schoolName}></img>
+        <img className="teamLogo" src="../../public/images/Team_logos/georgia-tech.120.png" alt=""></img>
       </div>
     )
   }
