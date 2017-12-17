95ea55c5b4c80699e01f009fea94a40c61eacc97
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2017-12-17 14:31:26 -0500

    query parameters

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 import { Matchup } from './react/views';
 
+function getParameterByName(parameter) {
+	var match = RegExp('[?&]' + parameter + '=([^&]*)').exec(window.location.search);
+	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+const school = getParameterByName("school")
+const matchupSchool = getParameterByName("matchupSchool")
+
+
 export default class App extends Component {
+  componentWillMount(){
+    this.setState({
+        schoolSlug: school,
+        matchupSchoolSlug: matchupSchool,            
+    })
+  }    
   render() {
     return (
-      <Matchup/>
+      <Matchup schoolSlug = {this.state.schoolSlug} matchupSchoolSlug = {this.state.matchupSchoolSlug} />
     );
   }
 }
