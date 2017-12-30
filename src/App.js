cf014811c4cd3bea3448f60d020afd3e0997d596
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2017-12-29 20:05:14 -0500

    got basic graphql working with apollo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 import { Matchup } from './react/views';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
+  cache: new InMemoryCache()
+});
+
 function getParameterByName(parameter) {
 	var match = RegExp('[?&]' + parameter + '=([^&]*)').exec(window.location.search);
 	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
@@ -10,17 +20,21 @@ function getParameterByName(parameter) {
 const school = getParameterByName("school")
 const matchupSchool = getParameterByName("matchupSchool")
 
-
 export default class App extends Component {
-  componentWillMount(){
-    this.setState({
-        schoolSlug: school,
-        matchupSchoolSlug: matchupSchool,            
-    })
-  }    
+  constructor(props) {
+    super(props);
+    this.state = {
+      schoolSlug: school,
+      matchupSchoolSlug: matchupSchool,
+  }}
+
   render() {
     return (
-      <Matchup schoolSlug = {this.state.schoolSlug} matchupSchoolSlug = {this.state.matchupSchoolSlug} />
-    );
+        <ApolloProvider client={client}>
+
+          <Matchup schoolSlug = {this.state.schoolSlug} matchupSchoolSlug = {this.state.matchupSchoolSlug} />
+          </ApolloProvider>
+
+    )
   }
 }
--- src/react/views/matchup/index.js
+++ src/react/views/matchup/index.js
@@ -1,55 +1,93 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import { ViewContainerFrame } from '../../components';
 import { MatchupHeader } from '../../components';
 import { MatchupFooter } from '../../components';
 
-export class Matchup extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props.schoolSlug)
-    console.log(this.props.matchupSchoolSlug)
-    this.state = {
-      "data": {
-        "team": {
-          "SchoolName": "Georgia Tech",
-          "SchoolSlug": "georgia-tech",
-          "MatchupSlug": "alabama",
-          "Branding": {
-            "HexColor": "#b39454"
-          },
-          "Matchup": {
-            "Wins": 21,
-            "Ties": 3,
-            "Losses": 28,
-            "WinStreakSchool": "Georgia Tech",
-            "WinStreak": 1,
-            "WinStreakYears": "1984",
-            "WinStreakSchoolColor": "#b39454",
-            "WinPercent": 0.43269230769231,
-            
-            "MatchupTeam": {
-              "SchoolName": "Alabama",
-              "Branding": {
-                "HexColor": "#a32136"
-              },
-              "Matchup": {
-                "Wins": 28,
-                "WinPercent": 0.56730769230769
-              }
-            }
-          }
+
+
+
+const myQuery = gql`query {
+  team(School: "georgia-tech", Matchup: "alabama") {
+    SchoolName
+    SchoolSlug
+    MatchupSlug
+    Branding {
+      HexColor
+    }
+    Matchup {
+      Wins
+      Ties
+      Losses
+      WinPercent
+      MatchupTeam {
+        SchoolName
+        Branding {
+          HexColor
+        }
+        Matchup {
+          Wins
+          WinPercent
         }
       }
     }
   }
- 
+  }`;
+
+const MyComponentWithData = graphql(myQuery)(props => 
+    <ViewContainerFrame>
+      <MatchupHeader matchUpData = {props}/>
+      {/* <MatchupFooter matchUpData = {props}/> */}
+    </ViewContainerFrame>
+);
+
+export class Matchup extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     "data": {
+  //       "team": {
+  //         "SchoolName": "Georgia Tech",
+  //         "SchoolSlug": "georgia-tech",
+  //         "MatchupSlug": "alabama",
+  //         "Branding": {
+  //           "HexColor": "#b39454"
+  //         },
+  //         "Matchup": {
+  //           "Wins": 21,
+  //           "Ties": 3,
+  //           "Losses": 28,
+  //           "WinStreakSchool": "Georgia Tech",
+  //           "WinStreak": 1,
+  //           "WinStreakYears": "1984",
+  //           "WinStreakSchoolColor": "#b39454",
+  //           "WinPercent": 0.43269230769231,
+            
+  //           "MatchupTeam": {
+  //             "SchoolName": "Alabama",
+  //             "Branding": {
+  //               "HexColor": "#a32136"
+  //             },
+  //             "Matchup": {
+  //               "Wins": 28,
+  //               "WinPercent": 0.56730769230769
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
 	render() {
 		return (
-      <ViewContainerFrame>
-        <MatchupHeader matchUpData = {this.state}/>
-        <MatchupFooter matchUpData = {this.state}/>
-      </ViewContainerFrame>
+
+      <MyComponentWithData>
+
+      </MyComponentWithData>
+
 		)
 	}
 }
